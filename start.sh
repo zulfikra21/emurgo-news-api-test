8f2b39f4bcde75ef22affbc394f4466cea16bed2
zulfikra96 <zulfikralahmudin@gmail.com>
2024-05-12 17:53:08 +0800

    update start

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ cargo build --release;
 
 mv target/release/actix-scaffolding ./actix-scaffolding;
 cargo clean;
-
+sudo docker network create 'main-network';
 sudo docker compose build && sudo docker compose up -d;
